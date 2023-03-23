7611f32e5963aad308ce57d6fd7a6c81d517d020
Davimrp <daviwendell2015@gmail.com>
2023-03-23 19:05:10 -0300

    Javascript da pagina Crew refatorado

--- JS/crew.js
+++ JS/crew.js
@@ -1,108 +1,71 @@
-const botoes = document.querySelectorAll('button')
+const botoes = document.querySelectorAll('button');
 const cargo = document.getElementById('cargo');
 const nome = document.getElementById('nome');
 const texto = document.getElementById('texto');
 const imagem = document.querySelector('#img');
-const infos = {
-    um: {
+
+const astronautas = [
+    {
+        id: 'um',
         cargo: 'Flight Engineer',
         nome: 'Anousheh Ansari',
-        texto: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.'
+        texto:
+            'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.',
+        imagem: '../assets/crew/image-anousheh-ansari.png',
     },
-    dois: {
+    {
+        id: 'dois',
         cargo: 'Pilot',
         nome: 'Victor Glover',
-        texto: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.'
+        texto:
+            'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.',
+        imagem: '../assets/crew/image-victor-glover.png',
     },
-    tres: {
+    {
+        id: 'tres',
         cargo: 'Commander',
         nome: 'Douglas Hurley',
-        texto: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.'
+        texto:
+            'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
+        imagem: '../assets/crew/image-douglas-hurley.png',
     },
-    quatro: {
+    {
+        id: 'quatro',
         cargo: 'Mission Specialist',
         nome: 'Mark Shuttleworth',
-        texto: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
-    }
-}
+        texto:
+            'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
+        imagem: '../assets/crew/image-mark-shuttleworth.png',
+    },
+];
 
-function alterarInfos(slideClicado){
+let currentIndex = 0;
+let timeoutId;
 
-    botoes.forEach((e)=>{
-        e.classList.remove('slide-active')
-    })
-    slideClicado.classList.add('slide-active')
-
-    if(slideClicado.id == 'um') slideUm(cargo, nome, texto, imagem)
-    if(slideClicado.id == 'dois') slideDois(cargo, nome, texto, imagem)
-    if(slideClicado.id == 'tres') slideTres(cargo, nome, texto, imagem)
-    if(slideClicado.id == 'quatro') slideQuatro(cargo, nome, texto, imagem)
-}
-
-function slideUm(cargo, nome, texto, imagem){
-    botoes.forEach((e)=>{
-        e.classList.remove('slide-active')
-    })
-    document.getElementById('um').classList.add('slide-active');
-
-    cargo.innerHTML = infos.um.cargo;
-    nome.innerHTML = infos.um.nome;
-    texto.innerHTML = infos.um.texto;
-    imagem.src = '../assets/crew/image-anousheh-ansari.png';
-    
-    setTimeout(() => {
-        slideDois(cargo, nome, texto, imagem)
+function mostrarAstronauta(index) {
+    const astronauta = astronautas[index];
+    cargo.innerHTML = astronauta.cargo;
+    nome.innerHTML = astronauta.nome;
+    texto.innerHTML = astronauta.texto;
+    imagem.src = astronauta.imagem;
+    botoes[currentIndex].classList.remove('slide-active');
+    botoes[index].classList.add('slide-active');
+    currentIndex = index;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        mostrarProximoAstronauta();
     }, 6000);
 }
 
-function slideDois(cargo, nome, texto, imagem){
-    botoes.forEach((e)=>{
-        e.classList.remove('slide-active')
-    })
-    document.getElementById('dois').classList.add('slide-active');
-
-    cargo.innerHTML = infos.dois.cargo;
-    nome.innerHTML = infos.dois.nome;
-    texto.innerHTML = infos.dois.texto;
-    imagem.src = '../assets/crew/image-victor-glover.png';
-
-    setTimeout(() => {
-        slideTres(cargo, nome, texto, imagem)
-    }, 6000);
-}
-function slideTres(cargo, nome, texto, imagem){
-    botoes.forEach((e)=>{
-        e.classList.remove('slide-active')
-    })
-    document.getElementById('tres').classList.add('slide-active');
-
-    cargo.innerHTML = infos.tres.cargo;
-    nome.innerHTML = infos.tres.nome;
-    texto.innerHTML = infos.tres.texto;
-    imagem.src = '../assets/crew/image-douglas-hurley.png';
-
-    setTimeout(() => {
-        slideQuatro(cargo, nome, texto, imagem)
-    }, 6000);
-}
-function slideQuatro(cargo, nome, texto, imagem){
-    botoes.forEach((e)=>{
-        e.classList.remove('slide-active')
-    })
-    document.getElementById('quatro').classList.add('slide-active');
-
-    cargo.innerHTML = infos.quatro.cargo;
-    nome.innerHTML = infos.quatro.nome;
-    texto.innerHTML = infos.quatro.texto;
-    imagem.src = '../assets/crew/image-mark-shuttleworth.png';
-
-    setTimeout(() => {
-        slideUm(cargo, nome, texto, imagem)
-    }, 6000);
+function mostrarProximoAstronauta() {
+    const proximoIndex = (currentIndex + 1) % astronautas.length;
+    mostrarAstronauta(proximoIndex);
 }
 
+botoes.forEach((botao, index) => {
+    botao.addEventListener('click', () => {
+        mostrarAstronauta(index);
+    });
+});
 
-botoes.forEach((e)=>{
-    e.addEventListener("click", e=> alterarInfos(e.target))
-})
-slideUm(cargo, nome, texto, imagem);
+mostrarAstronauta(currentIndex);
