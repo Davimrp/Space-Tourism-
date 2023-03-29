4eaf24d275187a94aa8d089c4afc1f967499908d
Davimrp <daviwendell2015@gmail.com>
2023-03-29 17:30:22 -0300

    responsividade pagina 4

--- JS/technology.js
+++ JS/technology.js
@@ -36,3 +36,24 @@ function alterarInfo(numeroClicado, botao){
 menu.forEach((e)=>{
     e.addEventListener('click', e=> alterarInfo(e.target.innerHTML -1, e.target));
 });
+
+
+const botaoMenu = document.querySelector('.fa-bars');
+const menuMobile = document.querySelector('.menu-mobile-active');
+const botaoSairMenu = document.getElementById('sair-menu')
+
+
+function abrirMenu(){
+    menuMobile.style.display = 'flex'
+    document.querySelector('main').style.display = 'none';
+    botaoMenu.style.opacity = '0';
+}
+botaoMenu.addEventListener('click', abrirMenu)
+
+
+function fecharMenu(){
+    menuMobile.style.display = 'none'
+    document.querySelector('main').style.display = 'flex';
+    botaoMenu.style.opacity = '1';
+}
+botaoSairMenu.addEventListener('click', fecharMenu)
