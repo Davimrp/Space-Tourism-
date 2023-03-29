8f13ab025713f2b5cfdd51744eb116db3d837627
Davimrp <daviwendell2015@gmail.com>
2023-03-29 16:43:37 -0300

    responsividade pagina 3

--- JS/crew.js
+++ JS/crew.js
@@ -70,3 +70,23 @@ botoes.forEach((botao, index) => {
 
 mostrarAstronauta(currentIndex);
 
+
+const botaoMenu = document.querySelector('.fa-bars');
+const menuMobile = document.querySelector('.menu-mobile-active');
+const botaoSairMenu = document.getElementById('sair-menu')
+
+
+function abrirMenu(){
+    menuMobile.style.display = 'flex'
+    document.querySelector('main').style.display = 'none';
+    botaoMenu.style.opacity = '0';
+}
+botaoMenu.addEventListener('click', abrirMenu)
+
+
+function fecharMenu(){
+    menuMobile.style.display = 'none'
+    document.querySelector('main').style.display = 'flex';
+    botaoMenu.style.opacity = '1';
+}
+botaoSairMenu.addEventListener('click', fecharMenu)
